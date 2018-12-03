25f622372c748fb6b6973012f78dec346c6561e9
xiongpeiji <450861493@qq.com>
2018-12-03 23:38:38 +0800

    代码提交

--- pages/experiencelist/experiencelist.js
+++ pages/experiencelist/experiencelist.js
@@ -1,11 +1,11 @@
-// pages/my/my.js
+// pages/experiencelist/experiencelist.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+
   },
 
   /**
@@ -13,73 +13,56 @@ Page({
    */
   onLoad: function (options) {
     wx.setNavigationBarTitle({
-      title: '我的'
+      title: '心得列表'
     });
   },
-  //编辑资料
-  editUserInfo(e){
-    wx.navigateTo({
-      url: '../editingmaterials/editingmaterials'
-    })
-  },
-  //手机认证
-  phoneAuth(e){
-    wx.navigateTo({
-      url: '../phoneauthentication/phoneauthentication'
-    })
-  },
-  //心得列表
-  xindeList(e){
-    wx.navigateTo({
-      url: '../experiencelist/experiencelist'
-    })
-  },
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
