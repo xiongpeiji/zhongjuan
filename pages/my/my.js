e2265020970f10c6352e31e45a646d9729cbc8f8
daipingshan <491906399@qq.com>
2018-12-18 21:10:56 +0800

    修改

--- pages/my/my.js
+++ pages/my/my.js
@@ -157,6 +157,7 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
+    app.setTabBarMsg()
     if(this.data.isLogin == true){
       this.setData({
         refresh: true
