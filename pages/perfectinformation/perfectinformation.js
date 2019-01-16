5b2ff09d58dd766e8109890d6e8559881fd1bbf0
xiongpeiji <450861493@qq.com>
2019-01-16 20:55:32 +0800

    修改样式

--- pages/perfectinformation/perfectinformation.js
+++ pages/perfectinformation/perfectinformation.js
@@ -10,6 +10,7 @@ Page({
   data: {
     id:0,
     donation_num:0,
+    express_data:{}
   },
 
   /**
@@ -24,9 +25,41 @@ Page({
       id:options.id,
       donation_num:options.donation_num,
     });
+    this.getData();
   },
 
 
+  //返回首页
+  goHome(e) {
+    wx.switchTab({
+      url: '../index/index'
+    })
+  },
+  //获取我要求捐信息
+  getData(e) {
+    let url = app_data.base + 'Donation/getExpress';
+    let params = { token: app_data.token };
+    http.Get({ url: url, params: params }).then((res) => {
+      if (res.code == 'success') {
+        this.setData({
+          express_data: res.data.data
+        })
+      }
+    });
+  },
+  //拨打快递电话
+  callNumber(e) {
+    let tel = e.currentTarget.dataset.tel;
+    wx.makePhoneCall({
+      phoneNumber: tel // 仅为示例，并非真实的电话号码
+    })
+  },
+  //查看更多物流信息
+  moreExpress(e) {
+    wx.navigateTo({
+      url: '../expresslist/expresslist'
+    });
+  },
   //完善物流
   perfectLogistics(e){
     wx.navigateTo({
