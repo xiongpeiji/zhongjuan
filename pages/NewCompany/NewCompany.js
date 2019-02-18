1d6804e38fa29916070545679c568877fed4f107
xiongpeiji <450861493@qq.com>
2019-02-18 23:46:53 +0800

    代码提交

---
+++ pages/NewCompany/NewCompany.js
@@ -0,0 +1,107 @@
+// pages/NewCompany/NewCompany.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    region: ['全部', '全部', '全部'],
+    customItem: '全部',
+    active: true,
+    province: '全部',
+    city: '全部',
+    area: '全部',
+  },
+  //省市区选择
+  bindRegionChange: function (e) {
+    this.setData({
+      region: e.detail.value,
+      province: e.detail.value[0],
+      city: e.detail.value[1],
+      area: e.detail.value[2],
+    })
+    this.getData({ refresh: true, is_first: true });
+  },
+  //回到顶部
+  goTop(e) {  // 一键回到顶部
+    if (wx.pageScrollTo) {
+      wx.pageScrollTo({
+        scrollTop: 0
+      })
+    } else {
+      wx.showModal({
+        title: '提示',
+        content: '当前微信版本过低，无法使用该功能，请升级到最新微信版本后重试。'
+      })
+    }
+  },
+  // 获取滚动条当前位置
+  onPageScroll: function (e) {
+    if (e.scrollTop > 100) {
+      this.setData({
+        floorstatus: true
+      });
+    } else {
+      this.setData({
+        floorstatus: false
+      });
+    }
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    wx.setNavigationBarTitle({
+      title: '单位'
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
